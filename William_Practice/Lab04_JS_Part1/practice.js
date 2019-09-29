7706d7b4ed564b0cb3433a1832b999ae52ee496f
wrt95 <wrt_95@hotmail.com>
2019-09-29 13:31:23 +0100

    Adding a css and js file for my homepage

--- William_Practice/Lab04_JS_Part1/practice.js
+++ William_Practice/Lab04_JS_Part1/practice.js
@@ -1,1 +1,10 @@
-// Missing
+
+document.getElementById("ClickMe").onclick = doFunction;
+
+var el = document.getElementById("clickMe");
+if (el.addEventListener)
+    el.addEventListener("click", doFunction, false);
+else if (el.attachEvent)
+    el.attachEvent('onclick', doFunction);
+
+    
