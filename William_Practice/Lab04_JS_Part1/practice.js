911529cb3c2ea1c02ca652a6e7f6fbc4606ba9f0
wrt95 <wrt_95@hotmail.com>
2019-09-30 08:13:55 +0100

    Adding a button to print the page

--- William_Practice/Lab04_JS_Part1/practice.js
+++ William_Practice/Lab04_JS_Part1/practice.js
@@ -1,8 +1,5 @@
 
-document.getElementById("ClickMe").onclick = doFunction;
 
-var el = document.getElementById("clickMe");
-if (el.addEventListener)
-    el.addEventListener("click", doFunction, false);
-else if (el.attachEvent)
-    el.attachEvent('onclick', doFunction);
+function print_current_page() {
+    window.print();
+}
