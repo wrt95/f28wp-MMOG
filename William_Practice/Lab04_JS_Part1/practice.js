c82f3a6006396cfe6d58c0ffd9a658a43dc7b4b4
wrt95 <wrt_95@hotmail.com>
2019-09-30 08:48:58 +0100

    implement basic calculator

--- William_Practice/Lab04_JS_Part1/practice.js
+++ William_Practice/Lab04_JS_Part1/practice.js
@@ -3,3 +3,39 @@
 function print_current_page() {
     window.print();
 }
+
+function addBy() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = (+num1) + (+num2);
+}
+
+function subtractBy() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 - num2;
+}
+
+function multiplyBy() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 / num2;
+}
+
+function remainder() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 % num2;
+}
+
+function doPower() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 ** num2;
+}
