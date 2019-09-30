69ca2299b2b6d6baff899ff55edd8f41328264f5
wrt95 <wrt_95@hotmail.com>
2019-09-30 10:43:02 +0100

    adding hover for buttons

--- William_Practice/Lab04_JS_Part1/practice.js
+++ William_Practice/Lab04_JS_Part1/practice.js
@@ -8,42 +8,42 @@ function print_current_page() {
 function addBy() {
     num1 = document.getElementById("firstNumber").value;
     num2 = document.getElementById("secondNumber").value;
-    document.getElementById("result").innerHTML = (+num1) + (+num2);
+    document.getElementById("calcResult").innerHTML = (+num1) + (+num2);
 }
 
 // function to do subraction on two input numbers
 function subtractBy() {
     num1 = document.getElementById("firstNumber").value;
     num2 = document.getElementById("secondNumber").value;
-    document.getElementById("result").innerHTML = num1 - num2;
+    document.getElementById("calcResult").innerHTML = num1 - num2;
 }
 
 // function to do multiplication on two input numbers
 function multiplyBy() {
     num1 = document.getElementById("firstNumber").value;
     num2 = document.getElementById("secondNumber").value;
-    document.getElementById("result").innerHTML = num1 * num2;
+    document.getElementById("calcResult").innerHTML = num1 * num2;
 }
 
 // function to do division on two input numbers
 function divideBy() {
     num1 = document.getElementById("firstNumber").value;
     num2 = document.getElementById("secondNumber").value;
-    document.getElementById("result").innerHTML = num1 / num2;
+    document.getElementById("calcResult").innerHTML = num1 / num2;
 }
 
 // function to get the remainder from two input numbers
 function remainder() {
     num1 = document.getElementById("firstNumber").value;
     num2 = document.getElementById("secondNumber").value;
-    document.getElementById("result").innerHTML = num1 % num2;
+    document.getElementById("calcResult").innerHTML = num1 % num2;
 }
 
 // function to do power on two input numbers
 function doPower() {
     num1 = document.getElementById("firstNumber").value;
     num2 = document.getElementById("secondNumber").value;
-    document.getElementById("result").innerHTML = num1 ** num2;
+    document.getElementById("calcResult").innerHTML = num1 ** num2;
 }
 
 // function to reverse an input string
