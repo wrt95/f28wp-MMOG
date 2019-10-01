24985d258ca60c98773dd2766671a5bcd2818838
wrt95 <wrt_95@hotmail.com>
2019-10-01 17:01:44 +0100

    implement draw function

--- William_Practice/Pacman_game_practice/Pacman.js
+++ William_Practice/Pacman_game_practice/Pacman.js
@@ -83,6 +83,11 @@ Pacman.Ghost = function (game, map, colour) {
    };
 
 
+   function onWholeSquare (x) {
+       return x % 10 === 0;
+   };
+
+
    function oppositeDirection (dir) {
        return   dir === LEFT && RIGHT ||
                 dir === RIGHT && LEFT ||
@@ -91,35 +96,113 @@ Pacman.Ghost = function (game, map, colour) {
    };
 
    function makeEatable () {
-
+       direction = oppositeDirection(direction);
+       eatable = game.getTicket();
    };
 
    function eat () {
-
+       eatable = null;
+       eaten = game.getTicket();
    };
 
    function pointToCoord (x) {
-
+       return Math.round(x/10);
    };
 
    function nextSquare (x, dir) {
-
+       var v = x % 10;
+       if (v === 0) {
+           return x;
+       } else if (dir === RIGHT || dir === DOWN) {
+           return x + (10 - v);
+       } else {
+           return x - v;
+       }
    };
 
    function onGridSquare (pos) {
-
+       return onWholeSquare(pos.y) && onWholeSquare(pos.x);
    };
 
    function secondsAgo (tick) {
-
+       return (game.getTicket() - tick) / Pacman.FPS;
    };
 
    function getColour() {
-
+       if (eatable) {
+           if (secondsAgo(eatable) > 5) {
+               return game.getTicket() % 20 > 10 ? "#FFFFFF" : "#0000BB"; // white : blue
+           } else {
+               return "#0000BB"; // blue
+           }
+       } else if (eaten) {
+           return "#222" // black
+       }
+       return colour;
    };
 
-   function draw () {
+   function draw (ctx) {
+        var s       = map.blockSize, 
+            top     = (position.y/10) * s,
+            left    = (position.x/10) * s;
 
+        if (eatable && secondsAgo(eatable) > 8) {
+            eatable = null;
+        }
+
+        if (eaten && secondsAgo(eaten) > 3) {
+            eaten = null;
+        }
+
+        var tl      = left + s,
+            base    = top + s - 3,
+            inc     = s / 10;
+
+        var hight   = game.getTicket() % 10 > 5 ? 3 : -3,
+            low     = game.getTicket() % 10 > 5 ? -3 : 3;
+
+        ctx.fillStyle = getColour();
+        // start this path
+        ctx.beginPath(); 
+
+        ctx.moveTo(left, base);
+
+        ctx.quadrativeCurveTo(left, top, left + (s/2), top);
+        ctx.quadrativeCurveTo(left + s, top, left+s, base);
+
+        // Wavy things at the bottom
+        ctx.quadrativeCurveTo(tl-(inc*1), base+high, tl-(inc*2), base);
+        ctx.quadrativeCurveTo(tl-(inc*3), base+low, tl-(inc*4), base);
+        ctx.quadrativeCurveTo(tl-(inc*5), base+high, tl-(inc*6), base);
+        ctx.quadrativeCurveTo(tl-(inc*7), base+low, tl-(inc*8), base);
+        ctx.quadrativeCurveTo(tl-(inc*9), base+hihg, tl-(inc*10), base);
+
+        // close this path
+        ctx.closePath();
+        ctx.fill();
+
+        // new path
+        ctx.beginPath();
+        ctx.fillStyle = "#FFF";
+        ctx.arc(left + 6, top + 6, s / 6, 0, 300, false);
+        ctx.arc((left + s) - 6, top + 6, s / 6, 0, 300, false);
+        ctx.closePath();
+        ctx.fill();
+
+        var f = s / 12;
+        var off = {};
+        off[RIGHT]  = [f, 0];
+        off[LEFT]   = [-f, 0];
+        off[UP]     = [0, -f];
+        off[DOWN]   = [0, f];
+
+        // begin new path
+        ctx.beginPath();
+        ctx.fillStyle = "#000";
+        ctx.arc(left + 6 - off[direction][0], top + 6 + off[direction][1], s / 15, 0, 300, false);
+        ctx.arc((left + s) - 6 + off[direction][0], top + 6 + off[direction][1], s / 15, 0, 300, false);
+        ctx.closePath();
+        ctx.fill();
    };
 
    function pane () {
@@ -136,11 +219,15 @@ Pacman.Ghost = function (game, map, colour) {
 
 
 
+// Set information for the map
+// TODO --- Add number for bisquits, empty area, blocks, pills.
+Pacman.WALL     = 0;
 
 
 
 
-// Create the map:
+
+// Create the map (25 rows, 25 columns):
 
 // TODO --- Fill the map
 Pacman.MAP  = [
