dc01ef6c9c831944e794fdc14dd00d9d33cc3b61
wrt95 <wrt_95@hotmail.com>
2019-10-24 13:23:12 +0100

    Adding jQuery

--- William_Practice/Practice_jQuery/test.js
+++ William_Practice/Practice_jQuery/test.js
@@ -1,18 +1,22 @@
-var p1 = $('#p1');
 
-$(document).keydown(function(e) {
-    var left = parseInt(p1.css('left') || 0);
-    var top = parseInt(p1.css('top') || 0);
-    if (e.which === 39 && ((left + p1.outerWidth() + 16) < 400)) {
-        p1.css('left', "+=16px");
-    } else if (e.which === 37 && (left > 0)) {
-        p1.css('left', '-=16px');
-    }
-    else if (e.which === 40 && ((top + p1.outerHeight() + 16) < 400)) {
-        p1.css('top', '+=16px'); 
-    }
-    else if (e.which === 38 && (top > 0)) {
-        p1.css('top', '-=16px');
-       // alert("test");
-    }
-});
+
+var pane = $('#pane'),
+    box = $('#box'),
+    w = pane.width() - box.width(),
+    d = {},
+    x = 3;
+
+function newv(v,a,b) {
+    var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+    return n < 0 ? 0 : n > w ? w : n;
+}
+
+$(window).keydown(function(e) { d[e.which] = true; });
+$(window).keyup(function(e) { d[e.which] = false; });
+
+setInterval(function() {
+    box.css({
+        left: function(i,v) { return newv(v, 37, 39); },
+        top: function(i,v) { return newv(v, 38, 40); }
+    });
+}, 20);
