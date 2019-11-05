98807ed37064a650ed68ee1512c94643fff57357
William Thorenfeldt <williamthorenfeldt@Williams-MacBook-Pro.local>
2019-11-05 15:41:53 +0000

    Adding comments to js file

--- Game/game.js
+++ Game/game.js
@@ -1,4 +1,8 @@
+// Variable for the score
+var score = 0;
 
+// Variable for the blue ball
+blueball = $('#blueball');
 
 // function to work the game timer 
 // help taken from - https://jsfiddle.net/wr1ua0db/17/ 
@@ -88,33 +92,38 @@ function startTimer () {
    
 
 
-// For the following function, i found help at this link: https://stackoverflow.com/questions/4950575/how-to-move-a-div-with-arrow-keys
-// It is used to make the ball move around in the gamearea.
+/* 
+For the following function, i found help at this link: https://stackoverflow.com/questions/4950575/how-to-move-a-div-with-arrow-keys
+It is used to make the ball move around in the gamearea.
+*/
 $(document).ready(function () {
+    // Set score initially to 0, and add it to the html.
     score = 0;
     $('#score').html(score);
 
+    // Variables for 
     var gameArea = $('#gameArea'),
-        blueball = $('#blueball'),
+       
        // redball = $('#redball'),              UNNCOMMENT FOR REDBALL
        // greenball = $('#greenball'),          UNNCOMMENT FOR GREENBALL
        // yellowball = $('#yellowball'),        UNNCOMMENT FOR YELLOWBALL
 
-        width = gameArea.width() - blueball.width(),    // the maximal left/top value for gameArea
-       // height = gameArea.height() - blueball.height(), 
-        keyPressed = {},                                // stores information of what key is pressed
-        distance = 10;                                   // the "speed", distance moved per intervall, in px
+        width = gameArea.width() - blueball.width(),    // widt:        The maximal left/top value for gameArea
+        keyPressed = {},                                // keyPressed:  Array to store information of which key is pressed
+        speed = 10;                                     // speed:       The distance moved per intervall, in px
 
-    // This function calculates the new top and left values based on
-    // the oldValue and the keyPressed. 
-    // we have two values, key1 and key2, so the ball can move diagonally.
+    /* 
+    This function calculates the new top and left values based on
+    the oldValue and the keyPressed. It basiclly updates the new position of the ball based on the
+    key that is pressed. We have two values, key1 and key2, so the ball can move diagonally.
+    */
     function newTopLeft(oldValue,key1,key2) {
-
         // creating a variable, setting it to the 
-        // parseInt version of the String oldValue, and the radix 10, MINUS
-        // keypressed of key1 (if the key pressed is in the array, return the distance, else return 0) PLUS
-        // keypressed of key2 (if the key pressed is in the array, return the distance, else return 0)
-        var n = parseInt(oldValue, 10) - (keyPressed[key1] ? distance : 0) + (keyPressed[key2] ? distance : 0);
+        // using parse int to convert the String String oldValue, with base 10, MINUS
+        // keypressed of key1 (if the key pressed is in the array, return the speed, else return 0) PLUS
+        // keypressed of key2 (if the key pressed is in the array, return the speed, else return 0)
+        // The reason for having to keys is to be able to move diagonally. 
+        var n = parseInt(oldValue, 10) - (keyPressed[key1] ? speed : 0) + (keyPressed[key2] ? speed : 0);
         
         // This expression ensures that the new value is in the permitted bounds.
         // if n is less than 0 return 0, 
@@ -123,56 +132,49 @@ $(document).ready(function () {
         return (n < 0 ? 0 : n > width ? width : n);
     } 
 
+    // This function sets the new values of the ball. It is done when a button is pressed. 
     $(window).keydown(function(e) { 
-        keyPressed[e.which] = true; // stores the key pressed, and set it to true
+        // The button pressed from the array is set to true. 
+        keyPressed[e.which] = true; 
+        // Update the left and top value of the ball in CSS. 
         blueball.css({
-            left: function(i,oldValue) { return newTopLeft(oldValue, 37, 39); },
-            top: function(i,oldValue) { return newTopLeft(oldValue, 38, 40); }
+            left: function(i,oldValue) { return newTopLeft(oldValue, 37, 39); }, // left arrow  = left,     right arrow = right
+            top: function(i,oldValue) { return newTopLeft(oldValue, 38, 40); }   // up arrow    = up,       down arrow  = down
         });
-        trackBall();
 
-    }); 
-
-    $(window).keyup(function(e) { 
-        keyPressed[e.which] = false;    // if the key is released, it´s not in use, so false
-    });  
-
-
-    // this function updates the interval. It is done every 20 milisecond. 
-    // if updates the left and top value of the blueball. The values is calculated by the 
-    // newTopLeft function. 
-   
-
-   /*     UNNCOMMENT FOLLOWING FOR ACTIVATING REDDBALL, GREENBALL AND YELLOWBALL
+ /*     UNNCOMMENT FOLLOWING FOR ACTIVATING REDDBALL, GREENBALL AND YELLOWBALL
         redball.css({
             left: function(i,oldValue) { return newTopLeft(oldValue, 65, 68); }, // a = left, d = right
             top: function(i,oldValue) { return newTopLeft(oldValue, 87, 83); } // w = up, s = down
         });
-
         greenball.css({
             left: function(i,oldValue) { return newTopLeft(oldValue, 74, 76); }, // j = left, l = right
             top: function(i,oldValue) { return newTopLeft(oldValue, 73, 75); } // i = up, k = down
         });
-
         yellowball.css({
             left: function(i,oldValue) { return newTopLeft(oldValue, 70, 72); }, // f = left, h = right
             top: function(i,oldValue) { return newTopLeft(oldValue, 84, 71); } // t = up, g = down
         });
-    */
+ */
 
-     // executed every 20 milisec
+        // Call track ball to get the position in the grid. 
+        trackBall();
+    }); 
+
+    // This function changed the buttons value to false.
+    $(window).keyup(function(e) { 
+        // if the key is released, it´s not in use, set to false
+        keyPressed[e.which] = false;    
+    });  
 });
 
 
-// ADD OBSTACVLE AND POSITION TO MAP <position, name>
-// position needs to be of all 4 walls. 
-
-
-
-// Creating an array for the keys. 
-var keyArray=new Array(37,38,39,40);
-
+/* 
+This function prevents the screen to move up and down if the arrows are pressed. 
+*/
 $(window).keydown(function(e) {
+    // Creating an array for the keys. 
+    var keyArray=new Array(37,38,39,40);
     // assign the key pressed to key. 
      var key = e.which;
       // if the key pressed is not in the array, prevent default, and return false. 
@@ -184,75 +186,21 @@ $(window).keydown(function(e) {
       return true;
 });
 
-/*
-// function that assigns the coins to random variables. 
-function makeCoins () {
-    // creating an element so we can show all. 
-    imgCoin = document.createElement("img");
-    var imgCoinsAttribute = document.createAttribute("class");
-    imgCoinsAttribute.value = "coin";
-    imgCoin.setAttributeNode(imgCoinsAttribute);
-    imgCoin.src = "Images/coin.png";
 
-    // variables for where coins might pop up. 
-    var xposCoin = Math.random()*90 | 5;
-    var yposCoin = Math.random()*90 | 5;
+// --- THE MAP ---
 
-    var coinsArray = new Array (15);
+// Variables for the different objects. 
+var obstacle = 0,   
+    free = 1,       
+    coin = 2;      
 
-    // coins position
-    for (var i = 0; i < 15; i++) {
-        imgCoin.style.position = "absolute";
-        imgCoin.style.left = xposCoin + '%';
-        imgCoin.style.top = yposCoin + '%';
-        coinsArray[i] = imgCoin;
-    }
-    
-    if (isPosFree) {
-        document.getElementById('gameArea').append(imgCoin);
-    }
-    return coinsArray;
-}*/
-
-
-// function to check if position is free.
-function isPosFree () {
-    var posFree = false;
-    for (var i = 0; i<10; i++) {
-        for (var j = 0; j <10; j++) {
-            if (gameArray[i][j] === free) {
-              posFree = true;
-            }
-        }
-    }
-    return posFree;
-}
-
-// function to get the shape of an obstacle
-function getObstacleShape () {
-    var size = 10 + '%',                   // size of the obstacle is 10% * 10%     
-        left = $("#ob*").position().left,  // left position
-        top = $("#ob*").position().top,    // top position
-        width = left + size,               // the width
-        height = top + size;               // the height
-
-    var bottomRight = width + height;      // bottom right position
-    var obstacleArray = [left, top, width, height, bottomRight];    // array with the positions
-
-    return obstacleArray;
-}
-
-
-var obstacle = 0,   // if position is taken
-    free = 1,       // if position is free
-    coin = 2;       // if position is coin
-
+// 2D array representing the game. 
 var gameArray = [
                 [2, 1, 1, 0, 1, 1, 1, 2, 0, 1],
                 [0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
                 [2, 0, 1, 0, 1, 1, 1, 1, 0, 1],
                 [1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
-                [0, 0, 0, 1, 1, 2, 1, 1, 0, 1],     // 2D array representing the game. 
+                [0, 0, 0, 1, 1, 2, 1, 1, 0, 1],     
                 [1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
                 [2, 0, 0, 1, 1, 1, 1, 1, 1, 2],
                 [1, 0, 1, 1, 0, 1, 0, 0, 0, 0],
@@ -261,40 +209,44 @@ var gameArray = [
                 ];
                 
 
+/* 
+This function tracks the ball. It uses Math.floor to round the numbers down, so we get the x and y position for the grid area. 
+It gets all the corners, and based on where it is positioned, it either kills the ball, or removes a coin / increment score. 
+*/
 function trackBall() {
+                                                                                        // --- THE LEFT TOP CORNER ---
+    var leftTopX = Math.floor((blueball.position().left) / 60);                         // LeftTopX is the left postion divided by 60 (600px for gameArea, 10 squares)
+    var leftTopY = Math.floor((blueball.position().top) / 60);                          // LeftTopY is the top postion divided by 60 (600px for gameArea, 10 squares)
 
-    var blueB = $("#blueball");
-
-    // Math.floor to round the value down.
-    var leftTopX = Math.floor((blueB.position().left) / 60);  // LeftTopX is the left postion divided by 60 (600px for gameArea, 10 squares)
-    var leftTopY = Math.floor((blueB.position().top) / 60);   // LeftTopY is the top postion divided by 60 (600px for gameArea, 10 squares)
-
-    // RightTopX is the left postion plus the width divided by 60 (600px for gameArea, 10 squares)
-    var rightTopX = Math.floor(((blueB.position().left) + (blueB.width()))/60);
-    // rightTopY is leftTopY
-    console.log(leftTopY + "," + rightTopX);
+                                                                                        // --- THE RIGHT TOP CORNER ---
+    var rightTopX = Math.floor(((blueball.position().left) + (blueball.width()))/60);   // RightTopX is the left postion plus the width divided by 60 (600px for gameArea, 10 squares)
+                                                                                        // rightTopY is leftTopY       (on same Y position.)
     
-    // LeftBottomY is the top postion plus the height divided by 60 (600px for gameArea, 10 squares)
-    var leftBottomY = Math.floor(((blueB.position().top) + (blueB.height()))/60);
-    // leftBottomX is leftTOpX
+                                                                                        // --- THE LEFT BOTTOM CORNER ---
+    var leftBottomY = Math.floor(((blueball.position().top) + (blueball.height()))/60); // LeftBottomY is the top postion plus the height divided by 60 (600px for gameArea, 10 squares)
+                                                                                        // leftBottomX is leftTOpX     (on same X position.)
 
-    // rightBottomY is leftBottomY
-    // rightBottomX is rightTopX
-
-
+                                                                                        // --- THE RIGHT BOTTOM CORNER ---
+                                                                                        // rightBottomY is leftBottomY (on same Y position.)
+                                                                                        // rightBottomX is rightTopX   (on same X position.) 
+// Following is used for debugging to track the grid position of the ball:
 //    console.log(left);
 //    console.log(top);
 
-    // if top left corner or top right corner or bottom left corner or bottom right corner is 2, remove the coin
+    // if top left corner is 2, remove the coin
     if (gameArray[leftTopY][leftTopX] === 2) {
         removeCoin(leftTopY, leftTopX);
+
     }
+    // if top right corner is 2, remove the coin
     else if (gameArray[leftTopY][rightTopX] === 2) {
         removeCoin(leftTopY, rightTopX);
     }
+    // if bottom left corner is 2, remove the coin
     else if (gameArray[leftBottomY][leftTopX] === 2) {
         removeCoin(leftBottomY, leftTopX);
     }
+    // if bottom right corner is 2, remove the coin
     else if (gameArray[leftBottomY][rightTopX] === 2) {
         removeCoin(leftBottomY, rightTopX);
     }
@@ -302,11 +254,32 @@ function trackBall() {
     // if top left corner or top right corner or bottom left corner or bottom right corner is 0, kill the ball.
     if (gameArray[leftTopY][leftTopX] === 0 || gameArray[leftTopY][rightTopX] === 0 || gameArray[leftBottomY][leftTopX] === 0 || gameArray[leftBottomY][rightTopX] === 0) {
         killBall();
-        //alert("YOU DIED!");
     }
 }
 
-var score = 0;
+/*
+This function "kills" the ball. 
+It changes the users score to 0, and sends the user back to start position. 
+*/
+function killBall() {
+    score = 0;
+    $('#score').html(score);
+    // set position of ball to startposition.
+    blueball.css({
+        left: "93%",
+        top: "2.5%"
+    });
+}
+
+/* 
+This function removes the ball and increments the score of the user. 
+NOT READY YET, MISSING IMPLEMENTATION OF COIN TO DISSAPEAR
+*/
+function removeCoin (y, x) {
+    score++;
+    $('#score').html(score);
+    gameArray[y][x] = 1;
+}
 
 // function for new game
 function newGame() {
@@ -318,27 +291,5 @@ function leaveGame(gameArea) {
     document.getElementById(gameArea).innerHTML= "Thanks for playing!";
 }
 
-function killBall() {
-    // alert("You died!");
-    // Set score to 0
-    score = 0;
-    $('#score').html(score);
-    // set position of ball to startposition.
-    $("#blueball").css({
-        left: 93.5 + "%",
-        top: "1%"
-    });
-}
-
-function removeCoin (y, x) {
-    score++;
-    $('#score').html(score);
-
-    gameArray[y][x] = 1;
-    
-}
-
-
 // NEED HELP WITH: 
-// - ball doesnt die when hitting from top, or from left. 
 // - how to make the coin disapear for 5 seconds. 
