5e28eafdd760a9a924f4f6e0d57cb8afd4a61b80
Peter Mathie <peter.a.mathie@gmail.com>
2019-11-15 12:19:33 +0000

    connecting to database

--- Game/routes/index.js
+++ Game/routes/index.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.jade');
+});
+
+router.get('/', function(req, res, next) {
+  res.render('gamepage');
 });
 
 module.exports = router;
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'sql2.freesqldatabase.com',
+    user: 'sql2310710',
+    password: 'aK1!vU9*',
+    database:'sql2310710'
+});
+
+var gameDB = {
+    USERNAME: 'ptr1',
+    SCORE:'91',
+    HIGHSCORE: '91',
+    GAMES_PLAYED: '1'
+}
+
+    database:'sql2310710'
+var query = connection.query('insert into sql2310710 set ?', sql2310710, function(err,result){
+
+});
---
+++ Game/routes/insert.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'sql2.freesqldatabase.com',
+    user: 'sql2310710',
+    password: 'aK1!vU9*',
+    database:'sql2310710'
+});
+
+var gameDB = {
+    USERNAME: 'ptr1',
+    SCORE:'91',
+    HIGHSCORE: '91',
+    GAMES_PLAYED: '1'
+}
+
+    database:'sql2310710'
+var query = connection.query('insert into sql2310710 set ?', sql2310710, function(err,result){
+
+});
