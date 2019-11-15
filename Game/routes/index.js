c4bab5a5bf9428e7c9b5b2516e9e6113d8533b50
Peter Mathie <peter.a.mathie@gmail.com>
2019-11-15 13:34:56 +0000

    Database should now connect to local via node.js :D

--- Game/routes/index.js
+++ Game/routes/index.js
@@ -11,24 +11,3 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-
-var mysql = require('mysql');
-
-var connection = mysql.createConnection({
-    host: 'sql2.freesqldatabase.com',
-    user: 'sql2310710',
-    password: 'aK1!vU9*',
-    database:'sql2310710'
-});
-
-var gameDB = {
-    USERNAME: 'ptr1',
-    SCORE:'91',
-    HIGHSCORE: '91',
-    GAMES_PLAYED: '1'
-}
-
-    database:'sql2310710'
-var query = connection.query('insert into sql2310710 set ?', sql2310710, function(err,result){
-
-});
