ab3309755f8e21d327b49f2f2c21f449b765d4cc
agm6 <agm6@hw.ac.uk>
2019-11-15 13:52:57 +0000

    fixed merge errors

--- Game/routes/index.js
+++ Game/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index.jade');
 });
 
-router.get('/', function(req, res, next) {
+router.get('/gamepage', function(req, res, next) {
   res.render('gamepage');
 });
 
