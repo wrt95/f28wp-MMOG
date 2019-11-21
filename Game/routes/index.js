3b535826ff911dd48c630fedfa6fd687c521990c
Peter Mathie <peter.a.mathie@gmail.com>
2019-11-21 13:01:00 +0000

    I git :))

--- Game/routes/index.js
+++ Game/routes/index.js
@@ -11,61 +11,43 @@ var con    =  mysql.createConnection({
 });
 
 var leaderboard;
+var usr;
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var UsrScore = "SELECT Users.USER_ID AS usrID, Users.USERNAME AS Username, Scores.score AS Score FROM Users JOIN Scores ON Scores.USER_ID = Users.USER_ID ORDER BY SCORE DESC";
-  var LdrBrd   = "SELECT Users.USERNAME AS Username, Scores.HIGHSCORE AS Highscore, Scores.GAMES_PLAYED AS Games_Played FROM Users JOIN Scores ON Users.USER_ID = Scores.USER_ID ORDER BY HIGHSCORE DESC LIMIT 5";
+  //var LdrBrd   = "SELECT Users.USERNAME AS Username, Scores.HIGHSCORE AS Highscore, Scores.GAMES_PLAYED AS Games_Played FROM Users JOIN Scores ON Users.USER_ID = Scores.USER_ID ORDER BY HIGHSCORE DESC LIMIT 5";
   var CnVis    = "SELECT VISIBLE AS Visible, COIN_ID AS CoinNum, COIN_X AS X, COIN_Y AS Y FROM Coins WHERE Coins.VISIBLE = 1";
   var CnInvis  = "SELECT VISIBLE AS Visible, COIN_ID AS CoinNum, COIN_X AS X, COIN_Y AS Y FROM Coins WHERE Coins.VISIBLE = 0";
-  // need to make functions to check if username is already in use and to generate a new user_id (if their name isn't taken)
-  //var insert   = "INSERT INTO Users (USERNAME, USER_ID) VALUES ('far', '117')"
-  //var insertS  = "INSERT INTO Scores (USER_ID, SCORE, HIGHSCORE) VALUES('110', '8','9')"
+  
+  var highscore = "SELECT HIGHSCORE FROM Scores"
+  var sqlUpdate = "UPDATE Scores.HIGHSCORE = ?"
+  /*
   con.query(UsrScore, function (err, result, fields) {
     if (err) throw err;
+    NameScore = result;
     console.log("User Scores",result);
-  });
-  con.query(LdrBrd, function (err, result, fields) {
-    if (err) throw err;
-    leaderboard = result;
-    console.log("Leaderboard",result);
-  });
-  con.query(CnVis, function (err, result, fields) {
-    if (err) throw err;
-    console.log("Visible Coins",result);
-  });
-  con.query(CnInvis, function (err, result, fields) {
-    if (err) throw err;
-    console.log("Invisible Coins",result);
-  });
-  /*con.query(insert, function (err, result) {
-    if (err) throw err;
-    console.log("Inserted");
-  });
-  con.query(insertS, function (err, result) {
-    if (err) throw err;
-    console.log("Inserted");
-  });
   */
-  con.query(UsrScore, function (err, result, fields) {
-    if (err) throw err;
-    for(i=0; i<result.length; i++)
-      console.log("User Scores",result[i]);
-  });
+ 
+ con.query(highscore, function(err, result, fields) {
+  if (err) throw err;
+  console.log("Inserted");
+ });
 });
 
 router.get('/', function(req, res, next) {
   console.log("Leaderboard:", leaderboard)
   res.render('index.jade', {ld: leaderboard});
 });
+router.get('/gamepage', function(req, res, next) {
+  console.log("Score", hgscr)
+  res.render('gamepage.jade', {hgscr})  
+});
 
 router.get('/gamepage', function(req, res, next) {
-  res.render('gamepage.jade');
+  console.log("usr:", NameScore)
+  res.render('gamepage', {ud: NameScore});
 });
 
-router.get('/instructions', function(req, res, next) {
-  res.render('instructions.jade');
-});
+module.exports = router;  
 
-module.exports = router;
