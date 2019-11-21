292c7fa8697f6b72963e1007046fec0d8965e02a
Peter Mathie <peter.a.mathie@gmail.com>
2019-11-21 18:38:33 +0000

    can pass variables from homepage.js to index.js to the sql database

--- Game/public/javascripts/homepage.js
+++ Game/public/javascripts/homepage.js
@@ -1,5 +1,18 @@
 // $(document).ready(function () {
-function login() {
-    var username = prompt("Please enter your username:")
-    var password = prompt("Enter enter your password:")
+
+function userin() {
+    //var username = prompt("Please enter your username:")
+    var username = "examplesarentcool ";
+    return username;
+}
+function passin(){
+    // var password = prompt("Enter enter your password:")
+    var password = "exposeyourself";
+    return password;
+}
+var username = userin();
+var password  = passin();
+module.exports = {
+    username,
+    password
 }
